5448d21fe375bcd43fdbb6831c5c56c1c884d7b9
danisaito <daniela.saito@gmail.com>
2025-08-20 20:04:05 -0300

    Add files via upload

--- 2025-1/site2/js/script.js
+++ 2025-1/site2/js/script.js
@@ -1,1 +1,7 @@
+const toggleButton = document.getElementById('toggleTheme');
+const body = document.body;
 
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  toggleButton.textContent = body.classList.contains('dark') ? '☀️' : '🌙';
+});
